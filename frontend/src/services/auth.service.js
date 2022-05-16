8030c6d6d73ecf0c566db34007c9d2c5ce890a1e
Jorge William <jorge_william_cardoso@yahoo.com.br>
2022-05-16 18:55:32 -0300

    Feat:  Services do frontend

---
+++ frontend/src/services/auth.service.js
@@ -0,0 +1,39 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-tabs */
+import axios from 'axios'
+//* * O prefixo do servidor já está configurado no package.json (proxy script) */
+// Tuto bezkoder ---> const API_URL = "http://localhost:5001/";
+
+class AuthService {
+	login(username, password) {
+		return axios
+			.post('/signin', {
+				username,
+				password
+			})
+			.then((response) => {
+				if (response.data.accessToken) {
+					localStorage.setItem('user', JSON.stringify(response.data))
+				}
+				return response.data
+			})
+	}
+
+	logout() {
+		localStorage.removeItem('user')
+	}
+
+	register(username, email, password) {
+		return axios.post('/signup', {
+			username,
+			email,
+			password
+		})
+	}
+
+	getCurrentUser() {
+		return JSON.parse(localStorage.getItem('user'))
+	}
+}
+
+export default new AuthService()
