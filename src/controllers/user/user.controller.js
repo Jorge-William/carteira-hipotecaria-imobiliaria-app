fea402fdb773ca3fe8d6057d6175e684859615f5
Jorge William <jorge_william_cardoso@yahoo.com.br>
2022-05-30 13:15:02 -0300

    Feat: Controller de usuário

---
+++ src/controllers/user/user.controller.js
@@ -0,0 +1,93 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs/dist/bcrypt");
+const process = require("process");
+const User = require("../../models/user.model");
+require("dotenv").config();
+// const keyToken = require("../../util");
+
+const secret = process.env.SECRET;
+// eslint-disable-next-line no-console
+console.log(secret);
+
+// ------------------------------------- Create ----------------------------------
+
+const create = async (req, res, next) => {
+  const {
+    name, lastName, email, password, type,
+  } = req.body;
+  try {
+    /**
+       * O método build() do sequelize prepara a query e a salva
+       * em uma variável nâo se conectando ao banco
+   * */
+    const user = User.build({
+      name,
+      lastName,
+      email,
+      password,
+      type,
+    });
+    const passwordHashed = await bcrypt.hash(user.password, 8);
+    user.password = passwordHashed;
+
+    // O método save() precisa ser assíncrono pois o mesmo se comunica com o banco de dados
+    await user.save();
+
+    // Para retornar os dados através do parametro res do express exemplo: res.user = user;
+    req.user = user;
+
+    // next() é chamado para que o fluxo constinue, retornando uma resposta para o router
+    next();
+  } catch (error) {
+    res.status(500).send({ errorMessage: error, message: "Não foi possivel criar o usuário" });
+  }
+};
+
+// ------------------------------------- Login -----------------------------------
+const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { email: `${req.body.email}` } });
+    if (user) {
+      const plainText = req.body.password;
+      const hashed = user.dataValues.password;
+      const passwordIsValid = await bcrypt.compare(plainText, hashed);
+      if (passwordIsValid) {
+        const token = jwt.sign(
+          {
+            id: user.dataValues.id,
+            auth: true,
+          },
+          secret,
+          {
+            expiresIn: process.env.TOKEN_EXPIRATION,
+          },
+        );
+        if (token) {
+          user.token = token;
+          user.save();
+        } else {
+          throw new Error();
+        }
+        req.user = user;
+        next();
+      } else {
+        throw new Error();
+      }
+    } else {
+      throw new Error();
+    }
+  } catch (error) {
+    res.status(401).send({ message: "Algo deu errado com o login." });
+  }
+};
+
+// ------------------------------------ Logout --------------------------------
+const logout = (req, res, next) => {
+  try {
+    next();
+  } catch {
+    res.send({});
+  }
+};
+
+module.exports = { login, create, logout };
