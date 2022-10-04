912a3052062c8edd506a2e55879c800186daf7f6
Jorge William <jorge_william_cardoso@yahoo.com.br>
2022-10-04 17:28:52 -0300

    Feat: Estado da requisição

--- frontend/src/pages/Usuarios.page.js
+++ frontend/src/pages/Usuarios.page.js
@@ -1,7 +1,25 @@
+import TabelaUsuarios from '../components/TabelaUsuarios'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 const Usuarios = () => {
+	const [userData, setUserData] = useState()
+
+	useEffect(() => {
+		return axios
+			.get('/usuarios')
+			.then((response) => {
+				setUserData(response)
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+	})
+
 	return (
 		<main>
 			<h1>Usuários</h1>
+			<TabelaUsuarios infoUser={userData} />
 		</main>
 	)
 }
