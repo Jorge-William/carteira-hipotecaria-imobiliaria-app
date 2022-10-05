69540f5b4e7dff613dfc48d10d42964be85b25b2
Jorge William <jorge_william_cardoso@yahoo.com.br>
2022-10-05 10:41:36 -0300

    Feat: dados que serão enviados ao front

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -5,25 +5,30 @@ const {
   create,
   logout,
 } = require("../../controllers/user/user.controller");
+const Usuario = require("../../models/user.model");
 
 const router = new express.Router();
 
 // ------------------------------------ Login ------------------------------------
 router.post("/login", login, async (req, res) => {
   const {
-/* eslint-disable */
-    id, name, lastName, token, type, usuario_id,
-    
-  } = req.user;
-  try {
-    // Por enquanto retorna todos os dados do usuario para o frontend
-    res.send({
-      message: "Bem vindo ",
-      id,
-      name,
-      lastName,
-      usuario_id,
-      /* eslint-enable */
+    /* eslint-disable */
+		id,
+		name,
+		lastName,
+		token,
+		type,
+		usuario_id
+	} = req.user
+	try {
+		// Por enquanto retorna todos os dados do usuario para o frontend
+		res.send({
+			message: 'Bem vindo ',
+			id,
+			name,
+			lastName,
+			usuario_id,
+			/* eslint-enable */
       type,
       token,
       userIsValid: true,
@@ -43,6 +48,24 @@ router.post("/logout", logout, (req, res) => {
   res.send({ message: "olá mundo" });
 });
 // -------------------------------------------------------------------------------
+
+// -------------------------------- Fetch usuarios -------------------------------
+router.get("/usuarios", async (req, res) => {
+  try {
+    const Usuarios = await Usuario.findAll();
+
+    console.log(Usuarios[0].dataValues);
+    const {
+      name, id, lastname, email, usuario_id, type,
+    } = Usuarios[0].dataValues;
+
+    res.send({
+      name, id, lastname, email, usuario_id, type,
+    });
+  } catch (error) {
+    res.status(500).send({ message: "algo deu errado com a requisição!" });
+  }
+});
 
 // -------------------------------- Create User ----------------------------------
 router.post("/admin/create", auth, create, async (req, res) => {
