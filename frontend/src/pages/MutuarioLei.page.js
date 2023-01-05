b20d4a1969120cd8165eaf71e01b65d414060a7b
Jorge William <jorge_william_cardoso@yahoo.com.br>
2023-01-05 11:21:17 -0300

    Feat: Botões para adicionar mutuário

--- frontend/src/pages/MutuarioLei.page.js
+++ frontend/src/pages/MutuarioLei.page.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import TabelaMutuarioLei from '../components/TabelaMutuarioLei'
+import { Link } from 'react-router-dom'
 
 const MutuarioLei = () => {
 	return (
 		<section>
-			<h1 className='mb-5'>Mutuário Lei</h1>
+			<div className='row'>
+				<div className='col-md'>
+					<h1 className=''>Mutuário Lei</h1>
+				</div>
+				<div id='' className='col-md-2'>
+					<Link to={`/mutuario/adicionar`}>
+						<div
+							className='btn btn-outline-success'
+							data-bs-toggle='collapse'
+							role='button'
+							aria-expanded='false'
+						>
+							Adicionar um Mutuário
+							<i className='bi bi-person-plus-fill ms-2' />
+						</div>
+					</Link>
+				</div>
+			</div>
 			<hr />
 			<TabelaMutuarioLei />
 			<br />
